5409a8dc0aa8a8a31355fb5cfa85e233c2f9d15c
ericndungutse <dav.ndungutse@gmail.com>
2025-05-17 23:01:59 +0200

    feat: create service clas that handles business logic

--- src/main/java/controller/TaskApiController.java
+++ src/main/java/controller/TaskApiController.java
@@ -10,14 +10,14 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import model.Task;
+import repository.TaskRepository;
+import service.TaskService;
 
 @WebServlet(name = "TaskApi", value = "/api/v1/tasks")
-public class TaskApi extends HttpServlet {
-    private String message;
+public class TaskApiController extends HttpServlet {
+ private final TaskService taskService = new TaskService(new TaskRepository());
 
-    public void init() {
-        message = "Hello World! again";
-    }
+    public void init() {}
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -47,55 +47,23 @@ public class TaskApi extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        String insertTaskSQL = "INSERT INTO tasks (title, description, due_date, status) VALUES (?, ?, ?, ?)";
-        try (BufferedReader reader = request.getReader();
-             Connection dbConnection = DBUtil.getConnection();
-             PreparedStatement stm = dbConnection.prepareStatement(insertTaskSQL, Statement.RETURN_GENERATED_KEYS);
-        ) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)  {
+        try(BufferedReader reader = request.getReader()) {
             StringBuilder sb = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
                 sb.append(line);
             }
             String json = sb.toString();
-
-            Task task = taskJsonToTaskMapper(json);
-
-            dbConnection.setAutoCommit(false);
-
-            stm.setString(1, task.getTitle());
-            stm.setString(2, task.getDescription());
-            stm.setDate(3, java.sql.Date.valueOf(task.getDueDate()));
-            stm.setString(4, task.getStatus());
-            int affectedRows = stm.executeUpdate();
-
-            if (affectedRows == 0) {
-                throw new SQLException("Creating task failed, no rows affected.");
-            }
-
-            // Get the id of newly created task
-            try (ResultSet generatedKeys = stm.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    UUID id = generatedKeys.getObject(1, UUID.class);
-                    task.setId(UUID.fromString(id.toString()));
-                } else {
-                    throw new SQLException("Creating task failed, no ID obtained.");
-                }
-            }
-
-            // Commit Transaction
-            dbConnection.commit();
+            Task newTask = taskService.createTask(taskJsonToTaskMapper(json));
 
             response.setContentType("application/json");
             response.setStatus(HttpServletResponse.SC_CREATED);
-            response.getWriter().write(taskObjToJsonMapper(task));
+            response.getWriter().write(taskObjToJsonMapper(newTask));
 
         } catch (Exception e) {
             e.printStackTrace();
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().write("{\"error\": \"" + e.getMessage() + "\"}");
         }
     }
 
